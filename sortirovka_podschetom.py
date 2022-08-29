56c51a6fd8c90e45e65fe60af1b2489d0b741063
Dauren Shalabayev <d.shalabayev@bsl.dev>
2022-08-29 13:02:07 +0600

    Удаление файла Singleton

--- sortirovka_podschetom.py
+++ sortirovka_podschetom.py
@@ -5,3 +5,15 @@ for number in a:
 for i in range(len(count)):
     if count[i] > 0:
         print((str(i) + ' ') * count[i],end='')
+
+print('end')
+s = 'abbczjfglskspsflasd;sg 6522 ()$#'
+letters = [0] * (len(s) +1)
+
+for i in s.lower():
+    if i>='a' and i<='z':
+        nomer = ord(i) - 97
+        letters[nomer] += 1
+
+for i in range(len(s)+1):
+    print(chr(i+97),letters[i])
